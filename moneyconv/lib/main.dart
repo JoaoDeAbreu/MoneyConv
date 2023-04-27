dc597b8acbc3a63d8fefe806dd1adb2b8233e3f0
João Gabriel de Abreu <371233@unoesc.lan>
2023-04-26 21:50:06 -0300

    quase pronto

--- moneyconv/lib/main.dart
+++ moneyconv/lib/main.dart
@@ -11,20 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'MoneyConv',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'MoneyConv'),
     );
   }
 }
@@ -32,84 +23,108 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+final TextEditingController _textFieldController = TextEditingController();
+
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  String _selectedOption = ' ';
+  double _multiplier = 1.0;
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      final String text = _textFieldController.text;
+      final double value = double.tryParse(text) ?? 0.0;
+      final double result = value * _multiplier;
+      final String mensagem =
+          'O valor convertido fica: ${result.toStringAsFixed(2)}';
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          final String text = _textFieldController.text;
+          final double value = double.tryParse(text) ?? 0.0;
+          final double result = value * _multiplier;
+          final String message =
+              'O valor convertido fica: ${result.toStringAsFixed(2)}';
+          return Column(
+            children: [
+              AlertDialog(
+                title: Center(child: const Text('Resultado')),
+                content: Text(message),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('OK'),
+                  ),
+                ],
+              ),
+            ],
+          );
+        },
+      );
+    });
+  }
+
+  void Option1() {
+    setState(() {
+      _selectedOption = 'Voce selecionou a opção Dolar';
+      _multiplier = 5.05;
+    });
+  }
+
+  void Option2() {
+    setState(() {
+      _selectedOption = 'Voce selecionou a opção Euro';
+      _multiplier = 5.60;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            ElevatedButton(style: ButtonStyle(),
+              onPressed: Option1,
+              child: const Text('Dolar'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            const SizedBox(height: 20),
+            ElevatedButton (
+              onPressed: Option2,
+              child: const Text('Euro',
+              ),
+            ),
+            Text(_selectedOption),
+            TextField(
+              style: TextStyle(letterSpacing: 1.5,
+              color: Color.fromRGBO(240, 95, 11, 1),
+              fontSize: 24),
+              controller: _textFieldController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(labelStyle: TextStyle(color: Color.fromARGB(255, 238, 17, 17)),
+                labelText: 'Digite o Valor em Reais',
+              ),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        tooltip: 'Calc',
+        child: const Icon(Icons.calculate),
+      ),
     );
   }
 }
